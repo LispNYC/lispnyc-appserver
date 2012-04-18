c5220ba64e93d11970c315ff0a42d6f8d0a13982
Heow Goodman <spamtrap@alphageeksinc.com>
2012-04-18 08:08:00 -0400

    added icons for news and useragent at java startup

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-java -jar start.jar 
+# note: useragent is set for google group access, otherwise we're locked out
+$JAVA_HOME/bin/java -Dhttp.agent="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:10.0.1) Gecko/20100101 Firefox/10.0.1" -jar start.jar 
